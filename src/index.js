74c9504f798d6f9cd3aade66d6c1185f9d73c47b
sumitshinde-84 <shindesumit217@gmail.com>
2023-02-10 13:07:03 +0530

    add style

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import "./styles.css"
-
-console.log('hellow')
+import "./styles/main.css";
+import "./styles/varriables.css";
+import "./styles/header.css";
+import "./styles/sidebar.css";
+import "./styles/mainContent.css";
+import "./styles/footer.css";
